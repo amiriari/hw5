25f4b2b9799d8b48aba85eef4772bfb0432bb9ed
Ariana Amiri <amiriari@usc.edu>
2025-04-26 06:57:21 +0000

    Completed HW5 Wordle and Schedwork

--- schedwork.cpp
+++ schedwork.cpp
@@ -22,6 +22,9 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 // Add prototypes for any helper functions here
 
+bool scheduleHelper(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts,
+                    DailySchedule& sched, vector<int>& shiftsAssigned, size_t day, size_t slot);
+
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -35,11 +38,53 @@ bool schedule(
     if(avail.size() == 0U){
         return false;
     }
+
+
+    size_t nDays = avail.size();
+    size_t nWorkers = avail[0].size();
+
     sched.clear();
-    // Add your code below
+    sched.resize(nDays);
+    for(auto& day : sched) {
+        day.clear();
+    }
 
+    vector<int> shiftsAssigned(nWorkers, 0);
 
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, shiftsAssigned, 0, 0);
+}
 
+bool scheduleHelper(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts,
+                    DailySchedule& sched, vector<int>& shiftsAssigned, size_t day, size_t slot)
+{
+    size_t nDays = avail.size();
+    size_t nWorkers = avail[0].size();
 
+    if(day == nDays) {
+        return true;
+    }
+
+    if(slot == dailyNeed) {
+        return scheduleHelper(avail, dailyNeed, maxShifts, sched, shiftsAssigned, day + 1, 0);
+    }
+
+    for(Worker_T worker = 0; worker < nWorkers; ++worker) {
+        if(avail[day][worker] && 
+           shiftsAssigned[worker] < maxShifts &&
+           find(sched[day].begin(), sched[day].end(), worker) == sched[day].end()) 
+        {
+            sched[day].push_back(worker);
+            shiftsAssigned[worker]++;
+
+            if(scheduleHelper(avail, dailyNeed, maxShifts, sched, shiftsAssigned, day, slot + 1)) {
+                return true;
+            }
+
+            sched[day].pop_back();
+            shiftsAssigned[worker]--;
+        }
+    }
+
+    return false;
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -14,6 +14,8 @@ using namespace std;
 
 // Add prototypes of helper functions here
 
+void buildWords(string& partial, int index, string floating, const set<string>& dict, set<string>& finalResults);
+
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -23,6 +25,49 @@ std::set<std::string> wordle(
 {
     // Add your code here
 
+    set<string> finalResults;
+    string partial = in;
+    buildWords(partial, 0, floating, dict, finalResults);
+    return finalResults;
+
 }
 
 // Define any helper functions here
+
+void buildWords(string& partial, int index, string floating, const set<string>& dict, set<string>& finalResults)
+{
+    if(index == partial.size()) {
+        // Base case: Full word created
+        if(dict.find(partial) != dict.end() && floating.empty()) {
+            finalResults.insert(partial);
+        }
+        return;
+    }
+
+    if(partial[index] != '-') {
+        buildWords(partial, index + 1, floating, dict, finalResults);
+    }
+    else {
+        set<char> tried;
+
+        for(int i = 0; i < floating.size(); ++i) {
+            char c = floating[i];
+            if(tried.find(c) == tried.end()) {
+                tried.insert(c);
+                partial[index] = c;
+                string newFloating = floating;
+                newFloating.erase(newFloating.begin() + i);
+                buildWords(partial, index + 1, newFloating, dict, finalResults);
+                partial[index] = '-'; 
+            }
+        }
+
+        for(char c = 'a'; c <= 'z'; ++c) {
+            if(tried.find(c) == tried.end()) {
+                partial[index] = c;
+                buildWords(partial, index + 1, floating, dict, finalResults);
+                partial[index] = '-'; 
+            }
+        }
+    }
+}
